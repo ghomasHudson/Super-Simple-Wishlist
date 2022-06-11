c5783b75afec15e463b255574ced3fc25486f00c
Thomas Hudson <g.t.hudson@durham.ac.uk>
2022-06-11 16:13:20 +0100

    Change db filename

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import json
 import os
 import glob
 
-CACHE_FILENAME = ".cache"
+CACHE_FILENAME = "db.json"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:101.0) Gecko/20100101 Firefox/101.0",
